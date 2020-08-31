8e30d7a85d5c85a58f8eb0b5b16f5de4df0b2aac
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-08-31 19:54:11 -0400

    Feature: adds function to retrieve favorited tweets
    the id's are sent to twitter api, and then the tweets are
    retrieved and returned

--- functions/handlers/twitter.js
+++ functions/handlers/twitter.js
@@ -1,21 +1,6 @@
 const { db, admin, T } = require('../utilities/admin');
 
-
-// var params = {
-//     q: '#nodejs',
-//     count: 10,
-//     result_type: 'recent',
-//     lang: 'en'
-//   }
-
-// T.get('search/tweets', params, function(err, data, response) {
-// if(!err){
-//     console.log(data)
-//     // This is where the magic will happen
-// } else {
-//     console.log(err);
-// }
-// })
+const _ = require('lodash');
 
 
 exports.searchForTweet = (req, res) => {
@@ -23,27 +8,66 @@ exports.searchForTweet = (req, res) => {
         q: req.body.query,
         count: req.body.count,
         result_type: 'recent',
-        lang: req.body.lang
-        // lang: 'en'
+        lang: req.body.language,
+        geo: "england"
     }
-
-    T.get('search/tweets', params, function(err, data, response) {
-        if(!err){
-            return res.json({data})
-        } else {
-            return res.status(400).json({err})
-            console.log(err);
-        }
-        })
+    let results = []
+    T.get('search/tweets', params, function (err, data, response) {
+       try {
+            results = data.statuses;
+       } catch (err) {
+           console.error(err)
+           return res.status(500).json({general: "something went wrong"})
+       } finally {
+            let length = results.length;
+            for (let i = 0; i < results.length; i++) {
+                results[i] = _.pick(results[i], [
+                    'id_str',
+                    'created_at',
+                    'text',
+                    'user["screen_name"]',
+                    'user["name"]',
+                    'user["id_str"]',
+                    'user["profile_image_url"]',
+                    'in_reply_to_status_id_str',
+                    'possibly_sensitive',
+                    'retweet_count',
+                    'favorite_count',
+                    'lang',
+                    'is_quote_status',
+                    'extended_entities["media"]'
+                ])
+            }
+            return res.json({length, results})
+       }
+                
+    })
+    
 }
 
 exports.getTweetById = (req, res) => {
         const params = {
-            id: "1300354705743282176"
+            id: req.params.tweetId
         }
 
         T.get('statuses/show/', params, function(err, data, response) {
             if(!err){
+                data = _.pick(data, [
+                    'id_str',
+                    'created_at',
+                    'text',
+                    'user["screen_name"]',
+                    'user["name"]',
+                    'user["id_str"]',
+                    'user["profile_image_url"]',
+                    'in_reply_to_status_id_str',
+                    'possibly_sensitive',
+                    'retweet_count',
+                    'favorite_count',
+                    'lang',
+                    'is_quote_status',
+                    'extended_entities["media"]'
+                ])
                 return res.json({data})
             } else {
                 return res.status(400).json({err})
@@ -55,7 +79,6 @@ exports.getTweetById = (req, res) => {
 exports.saveFavoriteTweet = async (req, res) => {
 
     const tweetId = req.params.tweetId;
-
     const user = db.collection("users").doc(`${email}`);
 
     await user.update({
@@ -81,13 +104,11 @@ exports.saveFavoriteTweet = async (req, res) => {
 exports.deleteFavoriteTweet = async (req, res) => {
 
     const tweetId = req.params.tweetId;
-
     const user = db.collection("users").doc(`${email}`);
 
     await user.update({
         favorites: admin.firestore.FieldValue.arrayRemove(tweetId)
     });
-
     let userDetails = {};
     return db.doc(`users/${email}`).get()
         .then((doc) => {
@@ -96,6 +117,60 @@ exports.deleteFavoriteTweet = async (req, res) => {
             }
             userDetails = doc.data();
             return res.json({userDetails});
+        })
+        .catch((error) => {
+            console.error(error);
+            return res.status(500).json({error: "Something went wrong"})
+        })
+}
+
+
+exports.getFavoriteTweets = (req, res) => {
+
+    let favorites = [];
+    db.doc(`users/${email}`).get()
+        .then( async (doc) => {
+            if(!doc.exists) {
+                return res.status(400).json({error: "Sorry, this user does not exist"})
+            }
+            favorites = doc.data().favorites;
+            let results = []
+            let count = 0;
+            favorites.forEach( async (tweetId) => {
+                await T.get(`statuses/show/${tweetId}`, function (err, data, response) {
+                    if(!err){
+                        count++
+                        console.log(count)
+                        results.push(data);
+                        if (count === favorites.length) {
+                            console.log(count)
+                            let length = results.length;
+                            for (let i = 0; i < results.length; i++) {
+                                results[i] = _.pick(results[i], [
+                                    'id_str',
+                                    'created_at',
+                                    'text',
+                                    'user["screen_name"]',
+                                    'user["name"]',
+                                    'user["id_str"]',
+                                    'user["profile_image_url"]',
+                                    'in_reply_to_status_id_str',
+                                    'possibly_sensitive',
+                                    'retweet_count',
+                                    'favorite_count',
+                                    'lang',
+                                    'is_quote_status',
+                                    'extended_entities["media"]'
+                                ])
+                            }
+                            return res.json({length, results})
+                        }
+                    } else {
+                        return res.status(400).json({err})
+                    }
+                })
+            })
+
         })
         .catch((error) => {
             console.error(error);
--- functions/index.js
+++ functions/index.js
@@ -3,14 +3,15 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 
-const { searchForTweet, getTweetById, saveFavoriteTweet, deleteFavoriteTweet } = require('./handlers/twitter');
+const { searchForTweet, getTweetById, saveFavoriteTweet, deleteFavoriteTweet, getFavoriteTweets } = require('./handlers/twitter');
 const { signup, login } = require('./handlers/users');
 const { auth } = require('./utilities/auth');
 
 app.post('/search', searchForTweet);
-app.post('/search/id', getTweetById);
+app.post('/search/:tweetId', getTweetById);
 app.post('/addfavorite/:tweetId', auth ,saveFavoriteTweet);
-app.post('/deletefavorite/:tweetId', auth ,deleteFavoriteTweet);
+app.post('/deletefavorite/:tweetId', auth, deleteFavoriteTweet);
+app.post('/getfavorites', auth, getFavoriteTweets);
 
 app.post('/signup', signup);
 app.post('/login', login);
