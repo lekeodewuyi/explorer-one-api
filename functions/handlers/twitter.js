14b187e9c4772a14121c5b53ea9b3d75910e7ea6
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-09-01 00:20:23 -0400

    Feature: changes how getFavoriteTweets is implemented
    the new function uses the lookup api route
    
    also refactors the lodash input into a reducer array

--- functions/handlers/twitter.js
+++ functions/handlers/twitter.js
@@ -2,47 +2,44 @@ const { db, admin, T } = require('../utilities/admin');
 
 const _ = require('lodash');
 
+const reducerArray = [
+    'id_str',
+    'created_at',
+    'text',
+    'user["screen_name"]',
+    'user["name"]',
+    'user["id_str"]',
+    'user["profile_image_url"]',
+    'in_reply_to_status_id_str',
+    'possibly_sensitive',
+    'retweet_count',
+    'favorite_count',
+    'lang',
+    'is_quote_status',
+    'extended_entities["media"]'
+]
 
 exports.searchForTweet = (req, res) => {
     const params = {
         q: req.body.query,
         count: req.body.count,
-        result_type: 'recent',
+        result_type: req.body.result_type,
         lang: req.body.language,
         geo: "england"
     }
     let results = []
-    T.get('search/tweets', params, function (err, data, response) {
-       try {
+
+    T.get('search/tweets/', params, function(err, data, response) {
+        if(!err){
             results = data.statuses;
-       } catch (err) {
-           console.error(err)
-           return res.status(500).json({general: "something went wrong"})
-       } finally {
-            let length = results.length;
             for (let i = 0; i < results.length; i++) {
-                results[i] = _.pick(results[i], [
-                    'id_str',
-                    'created_at',
-                    'text',
-                    'user["screen_name"]',
-                    'user["name"]',
-                    'user["id_str"]',
-                    'user["profile_image_url"]',
-                    'in_reply_to_status_id_str',
-                    'possibly_sensitive',
-                    'retweet_count',
-                    'favorite_count',
-                    'lang',
-                    'is_quote_status',
-                    'extended_entities["media"]'
-                ])
+                results[i] = _.pick(results[i], reducerArray)
             }
-            return res.json({length, results})
-       }
-                
+            return res.json({results})
+        } else {
+            return res.status(400).json({err})
+        }
     })
-    
 }
 
 exports.getTweetById = (req, res) => {
@@ -52,22 +49,7 @@ exports.getTweetById = (req, res) => {
 
         T.get('statuses/show/', params, function(err, data, response) {
             if(!err){
-                data = _.pick(data, [
-                    'id_str',
-                    'created_at',
-                    'text',
-                    'user["screen_name"]',
-                    'user["name"]',
-                    'user["id_str"]',
-                    'user["profile_image_url"]',
-                    'in_reply_to_status_id_str',
-                    'possibly_sensitive',
-                    'retweet_count',
-                    'favorite_count',
-                    'lang',
-                    'is_quote_status',
-                    'extended_entities["media"]'
-                ])
+                data = _.pick(data, reducerArray)
                 return res.json({data})
             } else {
                 return res.status(400).json({err})
@@ -128,47 +110,30 @@ exports.deleteFavoriteTweet = async (req, res) => {
 exports.getFavoriteTweets = (req, res) => {
 
     let favorites = [];
+    let results = [];
     db.doc(`users/${email}`).get()
         .then( async (doc) => {
             if(!doc.exists) {
                 return res.status(400).json({error: "Sorry, this user does not exist"})
             }
             favorites = doc.data().favorites;
-            let results = []
-            let count = 0;
-            favorites.forEach( async (tweetId) => {
-                await T.get(`statuses/show/${tweetId}`, function (err, data, response) {
-                    if(!err){
-                        count++
-                        console.log(count)
-                        results.push(data);
-                        if (count === favorites.length) {
-                            console.log(count)
-                            let length = results.length;
-                            for (let i = 0; i < results.length; i++) {
-                                results[i] = _.pick(results[i], [
-                                    'id_str',
-                                    'created_at',
-                                    'text',
-                                    'user["screen_name"]',
-                                    'user["name"]',
-                                    'user["id_str"]',
-                                    'user["profile_image_url"]',
-                                    'in_reply_to_status_id_str',
-                                    'possibly_sensitive',
-                                    'retweet_count',
-                                    'favorite_count',
-                                    'lang',
-                                    'is_quote_status',
-                                    'extended_entities["media"]'
-                                ])
-                            }
-                            return res.json({length, results})
+            let length = favorites.length;
+            let favoritesIds = favorites.toString();
+
+            const params = {
+                id: favoritesIds
+            }
+
+            return T.get(`statuses/lookup`, params, function (err, data, response) {
+                if(!err){
+                        for (let i = 0; i < data.length; i++) {
+                            data[i] = _.pick(data[i], reducerArray)
                         }
-                    } else {
-                        return res.status(400).json({err})
-                    }
-                })
+                        results = data;
+                        return res.json({length, results})
+                } else {
+                    return res.status(400).json({err})
+                }
             })
 
         })
